26cf8076521eb7beadd0d8c594461e22386f64dd
Pranesh <pranesh25joshi@gmail.com>
2024-10-07 12:38:31 +0530

    phantom button responsive

--- src/app/connectPhantomWallet/page.tsx
+++ src/app/connectPhantomWallet/page.tsx
@@ -35,36 +35,37 @@ export default function ConnectPhantomWallet() {
   }
 
   return (
-    <main className="bg-[#000000] shadow-lg rounded-lg p-6 max-w-md w-full text-center">
-        <h1 className="text-2xl font-bold text-white mb-4">Phantom Wallet Connection</h1>
-
-        {!walletAddress ? (
-          <button 
-            onClick={connectPhantomWallet} 
-            className="bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Connect Phantom Wallet
-          </button>
-        ) : (
-          <div className="bg-[#000000] p-6 rounded-lg shadow-md w-full max-w-md mx-auto">
-            <p className="text-green-600 font-semibold mb-2">Connected:</p>
-            <p className="text-green-600 font-semibold break-words">{walletAddress}</p>
-            <div className="flex justify-between gap-2 mt-6">
-              <button 
-                onClick={handleCreator} 
-                className="bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full"
-              >
-                Login As Creator
-              </button>
-              <button 
-                className="bg-gray-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 w-full"
-              >
-                Login As User
-              </button>
-            </div>
+    <main className="bg-[#000000] shadow-lg rounded-lg p-0 sm:p-6 max-w-[20rem] sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl text-center ">
+      <h1 className="text-2xl font-bold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-gray-500 via-white to-gray-500 mb-4">
+        Phantom Wallet Connection
+      </h1>
+    
+      {!walletAddress ? (
+        <button 
+          onClick={connectPhantomWallet} 
+          className="bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full"
+        >
+          Connect Phantom Wallet
+        </button>
+      ) : (
+        <div className="bg-[#000000] p-1 sm:p-6 rounded-lg shadow-md w-full max-w-md mx-auto">
+          <p className="text-green-600 font-semibold mb-2">Connected:</p>
+          <p className="text-green-600 font-semibold break-words">{walletAddress}</p>
+          <div className="flex flex-col sm:flex-row justify-between gap-2 mt-6">
+            <button 
+              onClick={handleCreator} 
+              className="bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full"
+            >
+              Login As Creator
+            </button>
+            <button 
+              className="bg-gray-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 w-full"
+            >
+              Login As User
+            </button>
           </div>
-          
-        )}
-      </main>
+        </div>
+      )}
+    </main>
   );
 }
