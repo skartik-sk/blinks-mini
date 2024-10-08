1c9654fa2b5212da45175a09ae0c2bb9ee6af916
Singupalli Kartik <skartik1706@gmail.com>
2024-10-09 01:58:20 +0530

    Refactor Access-Control-Allow headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = 
 {
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET, DELETE, PATCH, POST, PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Content-Length, Content-MD5, X-Api-Version" }
+                ]
+            }
+        ]
+    },
     images: {
         remotePatterns: [
             {
@@ -9,18 +22,7 @@ const nextConfig =
             },
           ],
     },
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: '/api/:path*',
-    //             destination: 'https://blinks-mini.vercel.app/:path*',
-    //         },  
-    //         {
-    //             source: '/api/:path*',
-    //             destination: 'https://blinks.knowflow.study/:path*',
-    //         },
-    //     ];
-    // },
+
 }
 
 export default nextConfig;
