91de27eac25aed5dbd415d42bebc63667dedcfc5
Pranesh <pranesh25joshi@gmail.com>
2024-10-25 13:41:42 +0530

    normal comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import ConnectPhantomWallet from "./connectPhantomWallet/page";
 export default function FrontPage() {
   return (
     <>
+    {/* the div below is the grid background and can be used in any other projetc too */}
       <div className="fixed left-0 top-0 -z-10 h-full w-full">
         <div className="relative h-full w-full bg-black">
           <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
@@ -15,7 +16,6 @@ export default function FrontPage() {
           <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]">
           </div>
         </div>
-        
       </div>
       <section className=" flex justify-center w-full py-12 md:py-24 lg:py-32 xl:py-48 ">
         <div className="container px-4 md:px-6">
