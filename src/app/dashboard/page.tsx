0a179421ab4d1cb083865bb7ab60a6ac6ed3de07
Pranesh <pranesh25joshi@gmail.com>
2024-10-26 17:41:20 +0530

    rpoduction error check

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 
 // import { Badge } from "@/components/ui/badge";
 // import { Zap } from "lucide-react";
