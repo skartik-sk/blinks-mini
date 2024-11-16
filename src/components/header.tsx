f54b3267aa72e001bb31d3e2bd2b7340120fcfb3
Pranesh <pranesh25joshi@gmail.com>
2024-11-16 23:00:01 +0530

    navbar fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
     return (
       
             <nav className="fixed top-0 left-0 w-full z-10 bg-black bg-opacity-5">
-                <div className="flex p-4 my-2 gap-28 md:gap-32 lg:gap-64 justify-between items-center  h-16">
+                <div className="flex p-4 my-2 md:gap-32 lg:gap-64 justify-between items-center  h-16">
                     <div className=" flex items-center justify-center sm:items-stretch sm:justify-start">
                         {/* logo thinggg */}
                         <div className="flex-shrink-0 flex items-center pl-4 lg:pl-6 ">
